77f445f139d34acb13d379a0201a3fc3b8ff7ec8
2457104-Simphiwe <2457104@students.wits.ac.za>
2024-04-22 10:49:46 +0200

    updated script.js

--- src/script.js
+++ src/script.js
@@ -1,12 +1,89 @@
-function toggleForms() {
-    var loginForm = document.getElementById('login-form');
-    var signupForm = document.getElementById('signup-form');
+import { AuthenticationService } from "./authenticationService.mjs";
+import { UserRepository } from "./userRepository.mjs";
+import { firestore, authentication } from "./firebase.mjs";
 
-    if (loginForm.style.display === "none") {
-        loginForm.style.display = "block";
-        signupForm.style.display = "none";
-    } else {
-        loginForm.style.display = "none";
-        signupForm.style.display = "block";
+const authenticationService = new AuthenticationService({
+  firebaseAuth: authentication,
+});
+
+const userRepository = new UserRepository({
+  database: firestore,
+});
+
+async function onSIgnUp(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const username = formData.get("username");
+  const role = formData.get("role");
+
+  const response = await authenticationService.RegisterUserWithEmailAndPassword(
+    {
+      email: email,
+      password: password,
     }
+  );
+
+  if (response !== "error") {
+    //
+    const user = {
+      id: response,
+      name: username,
+      email: email,
+      password: password,
+      role: role,
+    };
+
+    const dbResponse = await userRepository.createUser({ user: user });
+    alert(dbResponse);
+  }
 }
+
+async function onSignIn(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const role = formData.get("role");
+
+  const response = await authenticationService.SignInWithEmailAndPassword({
+    email: email,
+    password: password,
+  });
+
+  if (response !== "error") {
+    const user = await userRepository.getuserById({
+      id: response,
+    });
+
+    if (role !== user.role) {
+      alert("user not found");
+    } else {
+      //move to next page according to role
+      alert("signed in");
+    }
+  }
+}
+
+const signUpLink = document.getElementById("sign-up");
+const loginLink = document.getElementById("login");
+const loginForm = document.getElementById("login-form");
+const signUpForm = document.getElementById("signup-form");
+
+signUpLink.addEventListener("click", () => {
+  loginForm.style.display = "none";
+  signUpForm.style.display = "block";
+});
+
+loginLink.addEventListener("click", () => {
+  loginForm.style.display = "block";
+  signUpForm.style.display = "none";
+});
+
+signUpForm.addEventListener("submit", (e) => onSIgnUp(e));
+loginForm.addEventListener("submit", (e) => onSignIn(e));
