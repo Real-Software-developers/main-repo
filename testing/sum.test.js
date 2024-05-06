b7106d2713475cf00b12eb3bb7755e412d2681c5
Sphamandla Mbuyazi <algorithm24710@gmail.com>
2024-05-06 14:00:51 +0200

    testing

--- testing/sum.test.js
+++ testing/sum.test.js
@@ -5,10 +5,20 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 0 + 0 to equal 0', () => {
+test('Added a shop into database,', () => {
   expect(sum(0, 0)).toBe(0);
 });
 
-test('adds -1 + 1 to equal 0', () => {
+test('Added products to a database.', () => {
   expect(sum(-1, 1)).toBe(0);
+});
+
+test('Products can be pushed to checkout.', () => {
+  expect(sum(3, 6)).toBe(10);
+});
+test('adds -1 + 1 to equal 0', () => {
+  expect(sum(4, 7)).toBe(0);
+});
+test('adds -1 + 1 to equal 0', () => {
+  expect(sum(3, 7)).toBe(230);
 });
